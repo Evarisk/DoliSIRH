c895d1cc3ef914a49b50a86dcdfec4f9b020e79e
Nicolas Domenech <nicolas@eoxia.com>
2023-04-21 01:09:30 +0200

    #218 [JS] add: clean JS files for use Saturne JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,33 @@
 'use strict';
 
-var gulp         = require('gulp');
-var sass         = require('gulp-sass')(require('sass'));
-var rename       = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp   = require('gulp');
+const sass   = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
-var paths = {
-	scss_core : [ 'css/scss/**/*.scss', 'css/' ]
+const paths = {
+	scss_core: ['css/scss/**/*.scss', 'css/'],
+	js_backend: ['js/dolisirh.js', 'js/modules/*.js']
 };
 
 /** Core */
-gulp.task( 'scss_core', function () {
+gulp.task('scss_core', function() {
 	return gulp.src(paths.scss_core[0])
-		.pipe(sass({ 'outputStyle': 'expanded' }).on('error', sass.logError))
-		.pipe( autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}) )
-		.pipe(rename('./dolisirh.css'))
-		.pipe(gulp.dest(paths.scss_core[1]))
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(rename('./dolisirh.min.css'))
 		.pipe(gulp.dest(paths.scss_core[1]));
 });
 
+gulp.task('js_backend', function() {
+	return gulp.src(paths.js_backend)
+		.pipe(concat('dolisirh.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./js/'))
+});
+
 /** Watch */
-gulp.task( 'default', function () {
+gulp.task('default', function() {
 	gulp.watch(paths.scss_core[0], gulp.series('scss_core'));
+	gulp.watch(paths.js_backend[1], gulp.series('js_backend'));
 });
