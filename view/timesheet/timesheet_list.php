f43592027e291af04fd55b10c4656018afdf68f1
evarisk-micka <mickael.evarisk@gmail.com>
2023-06-06 10:03:42 +0200

    #277 [Timesheet] add: delete linked object after massaction delete

--- view/timesheet/timesheet_list.php
+++ view/timesheet/timesheet_list.php
@@ -252,6 +252,17 @@ if (empty($reshook)) {
 	}
 
 	// Mass actions
+    if ($massaction == 'delete' || ($action == 'delete' && $confirm == 'yes') && $permissiontodelete) {
+        $objectTmp = new TimeSheet($db);
+        if (is_array($toselect) && !empty($toselect)) {
+            foreach ($toselect as $toselectid) {
+                $objectTmp->fetch($toselectid);
+                $objectTmp->element = 'dolisirh_' . $objectTmp->element;
+                $objectTmp->deleteObjectLinked();
+            }
+        }
+    }
+
 	$objectclass = 'TimeSheet';
 	$objectlabel = 'TimeSheet';
 	$uploaddir = $conf->dolisirh->dir_output;
