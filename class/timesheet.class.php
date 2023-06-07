db9f162359539540ff89bbb1cebf2cb270832cd8
Nicolas Domenech <nicolas@eoxia.com>
2023-06-07 11:05:05 +0200

    #237 [TimeSheet] add: rework class for use SaturneObject

--- class/timesheet.class.php
+++ class/timesheet.class.php
@@ -16,20 +16,20 @@
  */
 
 /**
- * \file        class/timesheet.class.php
- * \ingroup     dolisirh
- * \brief       This file is a CRUD class file for TimeSheet (Create/Read/Update/Delete)
+ * \file    class/timesheet.class.php
+ * \ingroup dolisirh
+ * \brief   This file is a CRUD class file for TimeSheet (Create/Read/Update/Delete).
  */
 
-require_once DOL_DOCUMENT_ROOT.'/core/class/commonobject.class.php';
-require_once DOL_DOCUMENT_ROOT.'/core/lib/functions.lib.php';
+// Load Saturne Libraries.
+require_once __DIR__ . '/../../saturne/class/saturneobject.class.php';
 
 require_once __DIR__ . '/../../saturne/class/saturnesignature.class.php';
 
 /**
- * Class for TimeSheet
+ * Class for TimeSheet.
  */
-class TimeSheet extends CommonObject
+class TimeSheet extends SaturneObject
 {
 	/**
 	 * @var string ID of module.
@@ -58,9 +58,9 @@ class TimeSheet extends CommonObject
 	public $isextrafieldmanaged = 1;
 
 	/**
-	 * @var string String with name of icon for timesheet. Must be the part after the 'object_' into object_timesheet.png
+	 * @var string Name of icon for timesheet. Must be a 'fa-xxx' fontawesome code (or 'fa-xxx_fa_color_size') or 'timesheet@dolisirh' if picto is file 'img/object_timesheet.png'.
 	 */
-	public $picto = 'timesheet@dolisirh';
+	public string $picto = 'fontawesome_fa-calendar-check_fas_#d35968';
 
 	/**
 	 * @var array Label status of const.
@@ -198,180 +198,11 @@ class TimeSheet extends CommonObject
 	 */
 	public function __construct(DoliDB $db)
 	{
-		global $conf, $langs;
-
-		$this->db = $db;
-
-		if (empty($conf->global->MAIN_SHOW_TECHNICAL_ID) && isset($this->fields['rowid'])) {
-			$this->fields['rowid']['visible'] = 0;
-		}
-		if (empty($conf->multicompany->enabled) && isset($this->fields['entity'])) {
-			$this->fields['entity']['enabled'] = 0;
-		}
-
-		// Unset fields that are disabled
-		foreach ($this->fields as $key => $val) {
-			if (isset($val['enabled']) && empty($val['enabled'])) {
-				unset($this->fields[$key]);
-			}
-		}
-
-		// Translate some data of arrayofkeyval
-		if (is_object($langs)) {
-			foreach ($this->fields as $key => $val) {
-				if (!empty($val['arrayofkeyval']) && is_array($val['arrayofkeyval'])) {
-					foreach ($val['arrayofkeyval'] as $key2 => $val2) {
-						$this->fields[$key]['arrayofkeyval'][$key2] = $langs->trans($val2);
-					}
-				}
-			}
-		}
+        parent::__construct($db);
 	}
 
 	/**
-	 * Create object into database
-	 *
-	 * @param  User $user      User that creates
-	 * @param  bool $notrigger false=launch triggers after, true=disable triggers
-	 * @return int             0 < if KO, ID of created object if OK
-	 */
-	public function create(User $user, bool $notrigger = false): int
-	{
-		return $this->createCommon($user, $notrigger);
-	}
-
-	/**
-	 * Load object in memory from the database
-	 *
-	 * @param  int         $id  ID object
-	 * @param  string|null $ref Ref
-	 * @return int              0 < if KO, 0 if not found, >0 if OK
-	 */
-	public function fetch($id, string $ref = null): int
-	{
-		$result = $this->fetchCommon($id, $ref);
-		if ($result > 0 && !empty($this->table_element_line)) {
-			$this->fetchLines();
-		}
-		return $result;
-	}
-
-	/**
-	 * Load object lines in memory from the database
-	 *
-	 * @return int 0 < if KO, 0 if not found, >0 if OK
-	 */
-	public function fetchLines(): int
-	{
-		$this->lines = array();
-		return $this->fetchLinesCommon();
-	}
-
-	/**
-	 * Load list of objects in memory from the database.
-	 *
-	 * @param  string      $sortorder  Sort Order
-	 * @param  string      $sortfield  Sort field
-	 * @param  int         $limit      Limit
-	 * @param  int         $offset     Offset
-	 * @param  array       $filter     Filter array. Example array('field'=>'value', 'customurl'=>...)
-	 * @param  string      $filtermode Filter mode (AND/OR)
-	 * @return array|int               int <0 if KO, array of pages if OK
-	 * @throws Exception
-	 */
-	public function fetchAll(string $sortorder = '', string $sortfield = '', int $limit = 0, int $offset = 0, array $filter = array(), string $filtermode = 'AND')
-	{
-		dol_syslog(__METHOD__, LOG_DEBUG);
-
-		$records = array();
-
-		$sql = "SELECT ";
-		$sql .= $this->getFieldList('t');
-		$sql .= " FROM ".MAIN_DB_PREFIX.$this->table_element." as t";
-		if (isset($this->ismultientitymanaged) && $this->ismultientitymanaged == 1) {
-			$sql .= " WHERE t.entity IN (".getEntity($this->element).")";
-		} else {
-			$sql .= " WHERE 1 = 1";
-		}
-		// Manage filter
-		$sqlwhere = array();
-		if (count($filter) > 0) {
-			foreach ($filter as $key => $value) {
-				if ($key == 't.rowid') {
-					$sqlwhere[] = $key." = ".((int) $value);
-				} elseif (in_array($this->fields[$key]['type'], array('date', 'datetime', 'timestamp'))) {
-					$sqlwhere[] = $key." = '".$this->db->idate($value)."'";
-				} elseif ($key == 'customsql') {
-					$sqlwhere[] = $value;
-				} elseif (strpos($value, '%') === false) {
-					$sqlwhere[] = $key." IN (".$this->db->sanitize($this->db->escape($value)).")";
-				} else {
-					$sqlwhere[] = $key." LIKE '%".$this->db->escape($value)."%'";
-				}
-			}
-		}
-		if (count($sqlwhere) > 0) {
-			$sql .= " AND (".implode(" ".$filtermode." ", $sqlwhere).")";
-		}
-
-		if (!empty($sortfield)) {
-			$sql .= $this->db->order($sortfield, $sortorder);
-		}
-		if (!empty($limit)) {
-			$sql .= $this->db->plimit($limit, $offset);
-		}
-
-		$resql = $this->db->query($sql);
-		if ($resql) {
-			$num = $this->db->num_rows($resql);
-			$i = 0;
-			while ($i < ($limit ? min($limit, $num) : $num)) {
-				$obj = $this->db->fetch_object($resql);
-
-				$record = new self($this->db);
-				$record->setVarsFromFetchObj($obj);
-
-				$records[$record->id] = $record;
-
-				$i++;
-			}
-			$this->db->free($resql);
-
-			return $records;
-		} else {
-			$this->errors[] = 'Error '.$this->db->lasterror();
-			dol_syslog(__METHOD__.' '.join(',', $this->errors), LOG_ERR);
-
-			return -1;
-		}
-	}
-
-	/**
-	 * Update object into database
-	 *
-	 * @param  User $user      User that modifies
-	 * @param  bool $notrigger false=launch triggers after, true=disable triggers
-	 * @return int             0 < if KO, >0 if OK
-	 */
-	public function update(User $user, bool $notrigger = false): int
-	{
-		return $this->updateCommon($user, $notrigger);
-	}
-
-	/**
-	 * Delete object in database
-	 *
-	 * @param  User $user      User that deletes
-	 * @param  bool $notrigger false=launch triggers after, true=disable triggers
-	 * @return int             0 < if KO, >0 if OK
-	 */
-	public function delete(User $user, bool $notrigger = false): int
-	{
-		return $this->deleteCommon($user, $notrigger);
-	}
-
-	/**
-	 *  Delete a line of object in database
+	 * Delete a line of object in database
 	 *
 	 *	@param  User $user      User that delete
 	 *  @param  int  $idline	ID of line to delete
@@ -386,123 +217,6 @@ class TimeSheet extends CommonObject
 		}
 
 		return $this->deleteLineCommon($user, $idline, $notrigger);
-	}
-
-	/**
-	 *    Validate object
-	 *
-	 * @param  User      $user      User making status change
-	 * @param  int       $notrigger 1=Does not execute triggers, 0= execute triggers
-	 * @return int                  0 < if OK, 0=Nothing done, >0 if KO
-	 * @throws Exception
-	 */
-	public function validate(User $user, int $notrigger = 0): int
-	{
-		global $conf;
-
-		require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
-
-		$error = 0;
-
-		// Protection
-		if ($this->status == self::STATUS_VALIDATED) {
-			dol_syslog(get_class($this)."::validate action abandonned: already validated", LOG_WARNING);
-			return 0;
-		}
-
-		$now = dol_now();
-
-		$this->db->begin();
-
-		// Define new ref
-		if ((preg_match('/^\(?PROV/i', $this->ref) || empty($this->ref))) { // empty should not happen, but when it occurs, the test save life
-			$num = $this->getNextNumRef();
-		} else {
-			$num = $this->ref;
-		}
-		$this->newref = $num;
-
-		if (!empty($num)) {
-			// Validate
-			$sql = "UPDATE ".MAIN_DB_PREFIX.$this->table_element;
-			$sql .= " SET ref = '".$this->db->escape($num)."',";
-			$sql .= " status = ".self::STATUS_VALIDATED;
-			if (!empty($this->fields['date_validation'])) {
-				$sql .= ", date_validation = '".$this->db->idate($now)."'";
-			}
-			if (!empty($this->fields['fk_user_valid'])) {
-				$sql .= ", fk_user_valid = ".((int) $user->id);
-			}
-			$sql .= " WHERE rowid = ".($this->id);
-
-			dol_syslog(get_class($this)."::validate()", LOG_DEBUG);
-			$resql = $this->db->query($sql);
-			if (!$resql) {
-				dol_print_error($this->db);
-				$this->error = $this->db->lasterror();
-				$error++;
-			}
-
-			if (!$error && !$notrigger) {
-				// Call trigger
-				$result = $this->call_trigger('TIMESHEET_VALIDATE', $user);
-				if ($result < 0) {
-					$error++;
-				}
-				// End call triggers
-			}
-		}
-
-		if (!$error) {
-			$this->oldref = $this->ref;
-
-			// Rename directory if dir was a temporary ref
-			if (preg_match('/^\(?PROV/i', $this->ref)) {
-				// Now we rename also files into index
-				$sql = 'UPDATE '.MAIN_DB_PREFIX."ecm_files set filename = CONCAT('".$this->db->escape($this->newref)."', SUBSTR(filename, ".(strlen($this->ref) + 1).")), filepath = 'timesheet/".$this->db->escape($this->newref)."'";
-				$sql .= " WHERE filename LIKE '".$this->db->escape($this->ref)."%' AND filepath = 'timesheet/".$this->db->escape($this->ref)."' and entity = ".$conf->entity;
-				$resql = $this->db->query($sql);
-				if (!$resql) {
-					$error++; $this->error = $this->db->lasterror();
-				}
-
-				// We rename directory ($this->ref = old ref, $num = new ref) in order not to lose the attachments
-				$oldref = dol_sanitizeFileName($this->ref);
-				$newref = dol_sanitizeFileName($num);
-				$dirsource = $conf->dolisirh->dir_output.'/timesheet/'.$oldref;
-				$dirdest = $conf->dolisirh->dir_output.'/timesheet/'.$newref;
-				if (!$error && file_exists($dirsource)) {
-					dol_syslog(get_class($this)."::validate() rename dir ".$dirsource." into ".$dirdest);
-
-					if (@rename($dirsource, $dirdest)) {
-						dol_syslog("Rename ok");
-						// Rename docs starting with $oldref with $newref
-						$listoffiles = dol_dir_list($conf->dolisirh->dir_output.'/timesheet/'.$newref, 'files', 1, '^'.preg_quote($oldref, '/'));
-						foreach ($listoffiles as $fileentry) {
-							$dirsource = $fileentry['name'];
-							$dirdest = preg_replace('/^'.preg_quote($oldref, '/').'/', $newref, $dirsource);
-							$dirsource = $fileentry['path'].'/'.$dirsource;
-							$dirdest = $fileentry['path'].'/'.$dirdest;
-							@rename($dirsource, $dirdest);
-						}
-					}
-				}
-			}
-		}
-
-		// Set new ref and current status
-		if (!$error) {
-			$this->ref = $num;
-			$this->status = self::STATUS_VALIDATED;
-		}
-
-		if (!$error) {
-			$this->db->commit();
-			return 1;
-		} else {
-			$this->db->rollback();
-			return -1;
-		}
 	}
 
 	/**
@@ -526,128 +240,7 @@ class TimeSheet extends CommonObject
 	}
 
 	/**
-	 *	Set locked status
-	 *
-	 *	@param  User $user	    Object user that modify
-	 *  @param  int  $notrigger 1=Does not execute triggers, 0=Execute triggers
-	 *	@return	int				0 < if KO, 0=Nothing done, >0 if OK
-	 */
-	public function setLocked(User $user, int $notrigger = 0): int
-	{
-		return $this->setStatusCommon($user, self::STATUS_LOCKED, $notrigger, 'TIMESHEET_LOCKED');
-	}
-
-	/**
-	 *	Set archived status
-	 *
-	 *	@param  User $user	    Object user that modify
-	 *  @param  int  $notrigger 1=Does not execute triggers, 0=Execute triggers
-	 *	@return	int			    0 < if KO, >0 if OK
-	 */
-	public function setArchived(User $user, int $notrigger = 0): int
-	{
-		return $this->setStatusCommon($user, self::STATUS_ARCHIVED, $notrigger, 'TIMESHEET_ARCHIVED');
-	}
-
-	/**
-	 *  Return a link to the object card (with optionaly the picto)
-	 *
-	 *  @param  int     $withpicto              Include picto in link (0=No picto, 1=Include picto into link, 2=Only picto)
-	 *  @param  string  $option                 On what the link point to ('nolink', ...)
-	 *  @param  int     $notooltip              1=Disable tooltip
-	 *  @param  string  $morecss                Add more css on link
-	 *  @param  int     $save_lastsearch_value -1=Auto, 0=No save of lastsearch_values when clicking, 1=Save lastsearch_values whenclicking
-	 *  @return	string                          String with URL
-	 */
-	public function getNomUrl(int $withpicto = 0, string $option = '', int $notooltip = 0, string $morecss = '', int $save_lastsearch_value = -1): string
-	{
-		global $conf, $langs;
-
-		if (!empty($conf->dol_no_mouse_hover)) {
-			$notooltip = 1; // Force disable tooltips
-		}
-
-		$result = '';
-
-		$label = '<i class="fas fa-calendar-check" style="color: #d35968;"></i> <u>'.$langs->trans("TimeSheet").'</u>';
-		if (isset($this->status)) {
-			$label .= ' '.$this->getLibStatut(5);
-		}
-		$label .= '<br>';
-		$label .= '<b>'.$langs->trans('Ref').':</b> '.$this->ref;
-
-		$url = dol_buildpath('/dolisirh/view/timesheet/timesheet_card.php', 1).'?id='.$this->id;
-
-		if ($option != 'nolink') {
-			// Add param to save lastsearch_values or not
-			$add_save_lastsearch_values = ($save_lastsearch_value == 1 ? 1 : 0);
-			if ($save_lastsearch_value == -1 && preg_match('/list\.php/', $_SERVER["PHP_SELF"])) {
-				$add_save_lastsearch_values = 1;
-			}
-			if ($url && $add_save_lastsearch_values) {
-				$url .= '&save_lastsearch_values=1';
-			}
-		}
-
-		$linkclose = '';
-		if (empty($notooltip)) {
-			if (!empty($conf->global->MAIN_OPTIMIZEFORTEXTBROWSER)) {
-				$label = $langs->trans("ShowTimeSheet");
-				$linkclose .= ' alt="'.dol_escape_htmltag($label, 1).'"';
-			}
-			$linkclose .= ' title="'.dol_escape_htmltag($label, 1).'"';
-			$linkclose .= ' class="classfortooltip'.($morecss ? ' '.$morecss : '').'"';
-		} else {
-			$linkclose = ($morecss ? ' class="'.$morecss.'"' : '');
-		}
-
-		if ($option == 'nolink' || empty($url)) {
-			$linkstart = '<span';
-		} else {
-			$linkstart = '<a href="'.$url.'"';
-		}
-		$linkstart .= $linkclose.'>';
-		if ($option == 'nolink' || empty($url)) {
-			$linkend = '</span>';
-		} else {
-			$linkend = '</a>';
-		}
-
-		if ($withpicto) $result .= '<i class="fas fa-calendar-check" style="color: #d35968;"></i>' . ' ';
-        $result .= $linkstart;
-		if ($withpicto != 2) {
-			$result .= $this->ref;
-		}
-
-		$result .= $linkend;
-		//if ($withpicto != 2) $result.=(($addlabel && $this->label) ? $sep . dol_trunc($this->label, ($addlabel > 1 ? $addlabel : 0)) : '');
-
-		global $action, $hookmanager;
-		$hookmanager->initHooks(array('timesheetdao'));
-		$parameters = array('id'=>$this->id, 'getnomurl'=>$result);
-		$reshook = $hookmanager->executeHooks('getNomUrl', $parameters, $this, $action); // Note that $action and $object may have been modified by some hooks
-		if ($reshook > 0) {
-			$result = $hookmanager->resPrint;
-		} else {
-			$result .= $hookmanager->resPrint;
-		}
-
-		return $result;
-	}
-
-	/**
-	 *  Return the label of the status
-	 *
-	 *  @param  int     $mode 0=long label, 1=short label, 2=Picto + short label, 3=Picto, 4=Picto + long label, 5=Short label + Picto, 6=Long label + Picto
-	 *  @return	string 	      Label of status
-	 */
-	public function getLibStatut(int $mode = 0): string
-	{
-		return $this->LibStatut($this->status, $mode);
-	}
-
-	/**
-	 *  Return the status
+	 * Return the status
 	 *
 	 *  @param  int    $status Id status
 	 *  @param  int    $mode   0=long label, 1=short label, 2=Picto + short label, 3=Picto, 4=Picto + long label, 5=Short label + Picto, 6=Long label + Picto
@@ -679,50 +272,6 @@ class TimeSheet extends CommonObject
 	}
 
 	/**
-	 *	Load the info information in the object
-	 *
-	 *	@param  int   $id ID of object
-	 *	@return	void
-	 */
-	public function info(int $id)
-	{
-		$sql = "SELECT rowid, date_creation as datec, tms as datem,";
-		$sql .= " fk_user_creat, fk_user_modif";
-		$sql .= " FROM ".MAIN_DB_PREFIX.$this->table_element." as t";
-		$sql .= " WHERE t.rowid = ".($id);
-
-		$result = $this->db->query($sql);
-		if ($result) {
-			if ($this->db->num_rows($result)) {
-				$obj = $this->db->fetch_object($result);
-				$this->id = $obj->rowid;
-
-				$this->date_creation     = $this->db->jdate($obj->date_creation);
-                $this->date_modification = $this->db->jdate($obj->datem);
-			}
-
-			$this->db->free($result);
-		} else {
-			dol_print_error($this->db);
-		}
-	}
-
-	/**
-	 * Initialise object with example values
-	 * ID must be 0 if object instance is a specimen
-	 *
-	 * @return void
-	 */
-	public function initAsSpecimen()
-	{
-		// Set here init that are not common fields
-		// $this->property1 = ...
-		// $this->property2 = ...
-
-		$this->initAsSpecimenCommon();
-	}
-
-	/**
 	 * Create an array of lines
 	 *
 	 * @return array|int array of lines if OK, <0 if KO
@@ -743,76 +292,6 @@ class TimeSheet extends CommonObject
 			$this->lines = $result;
 			return $this->lines;
 		}
-	}
-
-	/**
-	 * Returns the reference to the following non-used object depending on the active numbering module.
-	 *
-	 *  @return string Object free reference
-	 */
-	public function getNextNumRef(): string
-	{
-		global $langs, $conf;
-		$langs->load("dolisirh@dolisirh");
-
-		if (empty($conf->global->DOLISIRH_TIMESHEET_ADDON)) {
-			$conf->global->DOLISIRH_TIMESHEET_ADDON = 'mod_timesheet_standard';
-		}
-
-		if (!empty($conf->global->DOLISIRH_TIMESHEET_ADDON)) {
-			$mybool = false;
-
-			$file = $conf->global->DOLISIRH_TIMESHEET_ADDON.".php";
-			$classname = $conf->global->DOLISIRH_TIMESHEET_ADDON;
-
-			// Include file with class
-			$dirmodels = array_merge(array('/'), $conf->modules_parts['models']);
-			foreach ($dirmodels as $reldir) {
-				$dir = dol_buildpath($reldir."core/modules/dolisirh/timesheet/");
-
-				// Load file with numbering class (if found)
-				$mybool |= @include_once $dir.$file;
-			}
-
-			if ($mybool === false) {
-				dol_print_error('', "Failed to include file ".$file);
-				return '';
-			}
-
-			if (class_exists($classname)) {
-				$obj = new $classname();
-				$numref = $obj->getNextValue($this);
-
-				if ($numref != '' && $numref != '-1') {
-					return $numref;
-				} else {
-					$this->error = $obj->error;
-					//dol_print_error($this->db,get_class($this)."::getNextNumRef ".$obj->error);
-					return "";
-				}
-			} else {
-				print $langs->trans("Error")." ".$langs->trans("ClassNotFound").' '.$classname;
-				return "";
-			}
-		} else {
-			print $langs->trans("ErrorNumberingModuleNotSetup", $this->element);
-			return "";
-		}
-	}
-
-	/**
-	 * Sets object to supplied categories.
-	 *
-	 * Deletes object from existing categories not supplied.
-	 * Adds it to non-existing supplied categories.
-	 * Existing categories are left untouched.
-	 *
-	 * @param  int[]|int $categories Category or categories IDs
-	 * @return float|int
-	 */
-	public function setCategories($categories)
-	{
-		return parent::setCategoriesCommon($categories, 'timesheet');
 	}
 }
 
